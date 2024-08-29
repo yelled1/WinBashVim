e4429c17c8a0c190ea8f749f8f9431403016c9d0
yelled1 <mailhyoon@gmail.com>
2024-08-29 15:27:42 -0400

    "added settings json for WindowsTerminal"

--- gather.sh
+++ gather.sh
@@ -3,3 +3,4 @@
 /usr/bin/cp -p ~/.bash_profile ./_bash_profile 
 /usr/bin/cp -p ~/scoop/*.sh .
 /usr/bin/cp -p /c/Users/alive/OneDrive/문서/WindowsPowerShell/Microsoft.PowerShell_profile.ps1 .
+/usr/bin/cp -p ~/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json .
