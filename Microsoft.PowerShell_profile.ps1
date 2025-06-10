a94a9b8b30aee018354a08dc2d6307c881600fc4
Sung Hoon <hoon@cmitnynj.com>
2025-06-10 15:38:43 -0400

    Update for chocolately usage

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,9 @@
-Set-Alias vi $ENV:UserProfile\scoop\apps\vim\current\vim.exe
-Set-Alias python ipython  
+# vim $PROFILE
+# https://medium.com/@devstefancho/display-git-branch-in-your-powershell-81b8b22ae3a7
+# PowerShellGet\Install-Module posh-git -Scope CurrentUser
+# PowerShellGet\Update-Module posh-git
+# Add-PoshGitToProfile
+
+Set-Alias vi vim
+Set-Alias python ipython
 Set-PSReadLineOption -EditMode Emacs
